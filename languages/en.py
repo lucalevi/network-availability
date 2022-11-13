24d3975946eb5733e33f8f0bbeb3b31c081a564c
Luca Iacolettig <iacolettig.luca@gmail.com>
2022-11-13 20:17:21 +0100

    language file en

---
+++ languages/en.py
@@ -0,0 +1,49 @@
+en = {
+    "language":{
+        "eng": "english",
+        "deu": "german",
+        "fra": "french",
+        "ita": "italian"
+    },
+    "common": {
+        "availabilityName" : 'availability',
+        "yearName" : 'year',
+        "monthName" : 'month',
+    },
+    "excel": {
+        "totalBlocks" : 'Availability_total_blocks',
+        "slowTracksYear" : 'Availability_slow_tracks_yearly',
+        "slowTracksMonth" : 'Availability_slow_tracks_monthly'
+    },
+    "sheet":{
+        "yearlyData": 'Yearly data',
+        "monthlyData": 'Monthly data',
+        "combined": 'Slow tracks + constr. sites',
+        "tracks": 'Slow tracks only',
+        'sites': 'Slow constr. sites only'
+    },
+    "chartTitle":{
+        "SlowTracksAllYear" : 'Yearly availability of the network \nSlow-speed segments\n(both slow-speed segments and slow-speed construction sites included)',
+        "SlowTracksOnlyTracksYear" : 'Yearly availability of the network \nSlow-speed segments\n(only slow-speed segments without slow-speed construction sites)',
+        "SlowTracksOnlySitesYear" : 'Yearly availability of the network \nSlow-speed segments\n(only slow-speed construction sites without slow-speed segments)',
+
+        "SlowTracksAllMonth" : 'Monthly availability of the network\nSlow-speed segments\n(both slow-speed segments and slow-speed construction sites included)',
+        "SlowTracksOnlyTracksMonth" : 'Monthly availability of the network \nSlow-speed segments\n(only slow-speed segments without slow-speed construction sites)',
+        "SlowTracksOnlySitesMonth" : 'Monthly availability of the network \nSlow-speed segments\n(only slow-speed construction sites without slow-speed segments)',
+
+        "TotalBlocksYear" : 'Yearly availability of the network\nTotal-block construction sites only',
+        "TotalBlocksMonth" : 'Monthly availability of the network\nTotal-block construction sites only',
+    },
+    "plot":{
+        "YearlySlowTracksAllFileName" : "Availability_slow_tracks_all_yearly",
+        "YearlySlowTracksOnlyTracksFileName" : "Availability_slow_tracks_only_tracks_yearly",
+        "YearlySlowTracksOnlySitesFileName" : "Availability_slow_tracks_only_sites_yearly",
+
+        "MonthlySlowTracksAllFileName" : "Availability_slow_tracks_all_monthly",
+        "MonthlySlowTracksOnlyTracksFileName" : "Availability_slow_tracks_only_tracks_monthly",
+        "MonthlySlowTracksOnlySitesFileName" : "Availability_slow_tracks_only_sites_monthly",
+
+        "YearlyTotalBlocksFileName" : "Availability_total_blocks_yearly",
+        "MonthlyTotalBlocksFileName" : "Availability_total_blocks_monthly"
+    }
+}
