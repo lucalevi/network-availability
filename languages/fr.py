195834a9a5d0300deac79820bfcc425b075e2b6d
Luca Iacolettig <iacolettig.luca@gmail.com>
2022-11-13 20:17:59 +0100

    rest languages files

---
+++ languages/fr.py
@@ -0,0 +1,49 @@
+fr = {
+    "langue":{
+        "eng" : "anglais",
+        "deu" : "allemand",
+        "fra" : "français",
+        "ita" : "italien"
+    },
+    "common" : {
+        "availabilityName" : "disponibilité",
+        "yearName" : "année",
+        "monthName" : "mois",
+    },
+    "excel" :{
+        "totalBlocks" : "Disponibilité_chantiers_fermeture_totale",
+        "slowTracksYear" : "Disponibilité_tronçons_à_vitesse_réduite_annuelle",
+        "slowTracksMonth" : "Disponibilité_tronçons_à_vitesse_réduite_mensuelle",
+    },
+    "sheet":{
+        "yearlyData" : "Données annuelles",
+        "monthlyData" : "Données mensuelles",
+        "combined" : "Tronçons lentes + chantiers",
+        "tracks" : 'Tronçons lentes seulement',
+        "sites" : "Chantiers lents seulement",
+    },
+    "chartTitle":{
+        "SlowTracksAllYear" : "Disponibilité annuelle du réseau\nSegments à vitesse lente\n(segments à vitesse lente et chantiers à vitesse lente inclus)",
+        "SlowTracksOnlyTracksYear" : "Disponibilité annuelle du réseau\nSegments à vitesse lente\n(uniquement les segments à vitesse lente sans les chantiers à vitesse lente)",
+        "SlowTracksOnlySitesYear" : "Disponibilité annuelle du réseau\nSegments à vitesse lente\n(uniquement les chantiers à vitesse lente sans les segments à vitesse lente)",
+
+        "SlowTracksAllMonth" : "Disponibilité mensuelle du réseau\nSegments à vitesse lente\n(segments à vitesse lente et chantiers à vitesse lente inclus)",
+        "SlowTracksOnlyTracksMonth" : "Disponibilité mensuelle du réseau\nSegments à vitesse lente\n(uniquement les segments à vitesse lente sans les chantiers à vitesse lente)",
+        "SlowTracksOnlySitesMonth" : "Disponibilité mensuelle du réseau\nSegments à vitesse lente\n(uniquement les chantiers à vitesse lente sans les segments à vitesse lente)",
+
+        "TotalBlocksYear" : "Disponibilité annuelle du réseau (uniquement les chantiers à fermeture totale)",
+        "TotalBlocksMonth" : "Disponibilité mensuelle du réseau (uniquement les chantiers à fermeture totale)",
+    },
+    "plot":{
+        "YearlySlowTracksAllFileName" : "Disponibilité_tronçoncs_lents_tous_annuelle",
+        "YearlySlowTracksOnlyTracksFileName" : "Disponibilité_tronçoncs_lents_seulement_tronçons_annuelle",
+        "YearlySlowTracksOnlySitesFileName" : "Disponibilité_tronçoncs_lents_seulement_chantiers_annuelle",
+
+        "MonthlySlowTracksAllFileName" : "Disponibilité_tronçoncs_lents_tous_mensuelle",
+        "MonthlySlowTracksOnlyTracksFileName" : "Disponibilité_tronçoncs_lents_seulement_tronçons_mensuelle",
+        "MonthlySlowTracksOnlySitesFileName" : "Disponibilité_tronçoncs_lents_seulement_chantiers_mensuelle",
+
+        "YearlyTotalBlocksFileName" : "Disponibilité_fermetures_complètes_annuelle",
+        "MonthlyTotalBlocksFileName" : "Disponibilité_fermetures_complètes_mensuelle",
+    }
+}
